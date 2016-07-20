397ea093f3556f832ad414241f62a86de4aa57fd
Eduardo Vieira <edu@pingu.tech>
2016-07-19 23:19:45 -0300

    Initial commit

---
+++ app/src/main/java/br/eco/washing4me/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package br.eco.washing4me.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.eco.washing4me.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
