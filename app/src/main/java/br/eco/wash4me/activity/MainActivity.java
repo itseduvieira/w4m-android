fbc3e8760a6522a7a41fcd54583e38755e73a150
Eduardo Vieira <edu@pingu.tech>
2016-08-02 02:56:44 -0300

    Adding Design Support Library, Material Drawer, and Floating Action Button examples for future customization.

--- app/src/main/java/br/eco/wash4me/activity/MainActivity.java
+++ app/src/main/java/br/eco/wash4me/activity/MainActivity.java
@@ -1,15 +1,88 @@
 package br.eco.wash4me.activity;
 
+import android.graphics.Color;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.LayoutInflaterCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.mikepenz.google_material_typeface_library.GoogleMaterial;
+import com.mikepenz.iconics.IconicsDrawable;
+import com.mikepenz.iconics.context.IconicsLayoutInflater;
 
 import br.eco.wash4me.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    NavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        LayoutInflaterCompat.setFactory(getLayoutInflater(), new IconicsLayoutInflater(getDelegate()));
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        bindViews();
+
+        setupViews();
+
+        setupMaterialDrawer();
+    }
+
+    private void bindViews() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+    }
+
+    private void setupViews() {
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setHomeAsUpIndicator(new IconicsDrawable(this)
+                .icon(GoogleMaterial.Icon.gmd_menu)
+                .color(Color.WHITE)
+                .sizeDp(16));
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                menuItem.setChecked(true);
+                drawerLayout.closeDrawers();
+                Snackbar.make(drawerLayout, menuItem.getTitle(), Snackbar.LENGTH_LONG).show();
+                return true;
+            }
+        });
+    }
+
+    private void setupMaterialDrawer() {
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
