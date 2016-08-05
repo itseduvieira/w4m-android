7cb4bba690b34312439194d3a977279b1de468a2
Eduardo Vieira <edu@pingu.tech>
2016-08-05 10:13:47 -0300

    Creating login layout.

--- app/src/main/java/br/eco/wash4me/activity/LoginActivity.java
+++ app/src/main/java/br/eco/wash4me/activity/LoginActivity.java
@@ -3,6 +3,7 @@ package br.eco.wash4me.activity;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -336,6 +337,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
             if (success) {
                 finish();
+
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
--- app/src/main/java/br/eco/wash4me/activity/Splash.java
+++ app/src/main/java/br/eco/wash4me/activity/Splash.java
@@ -17,7 +17,7 @@ public class Splash extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(Splash.this, MainActivity.class));
+                startActivity(new Intent(Splash.this, LoginActivity.class));
             }
         }, 3000);
     }
